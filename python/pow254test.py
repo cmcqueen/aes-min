c7fc3051a28f652c8b72f6c88a46bb6f4e487848
Craig McQueen <craig.mcqueen@beamcommunications.com>
2014-12-18 15:18:53 +1100

    Updated Python tests of 254 addition chains for Galois inverse in GF(2^8).

--- python/pow254test.py
+++ python/pow254test.py
@@ -27,7 +27,7 @@ def brauer_gen(fd):
         if tag == "b":
             nums = tuple( int(x) for x in num_line.split(" ") )
             deltas = tuple(delta_gen(nums))
-            indices = tuple( index(nums, x) - i for i, x in enumerate(delta_gen(nums)) )
+            indices = tuple( index(nums, x) for i, x in enumerate(delta_gen(nums)) )
             yield indices
 
 def main():
@@ -35,7 +35,13 @@ def main():
     with open("ac0254.txt") as f:
         for a in brauer_gen(f):
             #print(a)
-            worst_delta = min(a)
+            count_zeros = sum(1 if i==x else 0 for i, x in enumerate(a))
+            non_zero_indices_for_min = tuple(99 if i==x else x for i, x in enumerate(a))
+            non_zero_indices_for_max = tuple(0 if i==x else x for i, x in enumerate(a))
+            delta_indices = tuple(i - x for i, x in enumerate(a))
+            worst_delta = max(delta_indices)
+            min_non_zero_index = min(non_zero_indices_for_min)
+            max_non_zero_index = max(non_zero_indices_for_max)
             #print("    ", worst_delta)
 
             if 0:
@@ -43,8 +49,8 @@ def main():
                     best = (worst_delta, a)
                     print(best)
             if 1:
-                if worst_delta >= -3:
-                    print((worst_delta, a))
+                if worst_delta <= 11 and min_non_zero_index >= 1 and max_non_zero_index <= 3 and count_zeros >= 7:
+                    print((worst_delta, count_zeros, (min_non_zero_index, max_non_zero_index), a))
 
     #print("Best:", best)
             
