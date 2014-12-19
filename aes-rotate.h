245f7860b834b97b8c44785fe799990750e23b35
Craig McQueen <craig.mcqueen@beamcommunications.com>
2014-12-19 12:00:21 +1100

    Comments on aes_rotate_left_uint8() regarding optimisation.

--- aes-rotate.h
+++ aes-rotate.h
@@ -5,9 +5,16 @@
 
 #include <stdint.h>
 
+/* Hopefully the compiler reduces this to a single rotate instruction.
+ * However in testing with gcc on x86-64, it didn't happen. But it is target-
+ * and compiler-specific. Examine code produced on the target platform.
+ * Alternatively for a particular platform:
+ *     - Use an intrinsic 8-bit rotate function provided by the compiler.
+ *     - Use inline assembler.
+ */
 static inline uint8_t aes_rotate_left_uint8(uint8_t a, uint_fast8_t num_bits)
 {
-    return (a << num_bits) | (a >> (8u - num_bits));
+    return ((a << num_bits) | (a >> (8u - num_bits)));
 }
 
 #endif /* !defined(AES_MUL2_H) */
