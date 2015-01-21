9c7e837856c12817ef929b4fd6dce3badec76d65
Craig McQueen <craig@mcqueen.id.au>
2015-01-21 21:50:03 +1100

    Improve Python parser of AES KAT. Support multiple file inputs.
    From the AES KAT (Known Answer Tests), we can also use
    ECBVarKey128.rsp and ECBVarTxt128.rsp.

--- python/parse-vectors.py
+++ python/parse-vectors.py
@@ -16,6 +16,12 @@ def byte_string_to_c_array_init(byte_string):
 def vectors_iter(fileobj):
     for line in fileobj:
         line = line.strip()
+        if "[ENCRYPT]" in line:
+            break
+    for line in fileobj:
+        line = line.strip()
+        if "[DECRYPT]" in line:
+            break
         if line.startswith("COUNT"):
             parts = line.split("=")
             count = int(parts[1].strip())
@@ -31,35 +37,42 @@ def vectors_iter(fileobj):
                 value = codecs.decode(valuestr, "hex")
                 test_data[key] = value
 
+def files_vectors_iter(filenames):
+    for setnum, filename in enumerate(filenames):
+        with open(filename, "r") as f:
+            for test_data in vectors_iter(f):
+                test_data['set'] = setnum
+                yield test_data
+
 def main():
     import sys
 
-    filename = sys.argv[1]
-    with open(filename, "r") as f:
-        vectors_list = []
-        for test_data in vectors_iter(f):
-            #pprint(test_data)
-            vector_prefix = "count{}".format(test_data['count'])
-            for key in ('key', 'plain', 'cipher'):
-                if key in test_data:
-                    array_data = byte_string_to_c_array_init(test_data[key])
-                    print("const uint8_t {}{}[] = {{ {} }};".format(vector_prefix, key, array_data))
+    filenames = sys.argv[1:]
+    vectors_list = []
+    for test_data in files_vectors_iter(filenames):
+        #pprint(test_data)
+        vector_prefix = "set{}count{}".format(test_data['set'], test_data['count'])
+        for key in ('key', 'plain', 'cipher'):
+            if key in test_data:
+                array_data = byte_string_to_c_array_init(test_data[key])
+                print("const uint8_t {}{}[] = {{ {} }};".format(vector_prefix, key, array_data))
 
-            print("const vector_data_t {} = {{".format(vector_prefix))
-            print("    .count = {},".format(test_data['count']))
-            for key in ('key', 'plain', 'cipher'):
-                if key in test_data:
-                    print("    .{} = {}{},".format(key, vector_prefix, key))
-                else:
-                    print("    .{} = NULL,".format(key))
-            print("};")
-            print()
-            vectors_list.append(vector_prefix)
-
-        print("const vector_data_t * const test_vectors[] = {")
-        for vector_name in vectors_list:
-            print("    &{},".format(vector_name))
+        print("const vector_data_t {} = {{".format(vector_prefix))
+        print("    .set = {},".format(test_data['set']))
+        print("    .count = {},".format(test_data['count']))
+        for key in ('key', 'plain', 'cipher'):
+            if key in test_data:
+                print("    .{} = {}{},".format(key, vector_prefix, key))
+            else:
+                print("    .{} = NULL,".format(key))
         print("};")
+        print()
+        vectors_list.append(vector_prefix)
+
+    print("const vector_data_t * const test_vectors[] = {")
+    for vector_name in vectors_list:
+        print("    &{},".format(vector_name))
+    print("};")
 
 if __name__ == "__main__":
     main()
